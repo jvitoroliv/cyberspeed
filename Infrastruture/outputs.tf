64a6265b5d8e585b607ad5a21b01608e420ee13e
João Vitor Oliveira <joao.oliveira@pyypl.com>
2024-03-19 01:47:46 +0400

    deleting stack

--- Infrastruture/outputs.tf
+++ Infrastruture/outputs.tf
@@ -29,22 +29,22 @@ output "public_subnet_1c" {
 
 
 ##### EKS Module ######
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
 
-output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
-}
+# output "cluster_certificate_authority_data" {
+#   value = module.eks.cluster_certificate_authority_data
+# }
 
-output "cluster_id" {
-  value = module.eks.cluster_id
-}
+# output "cluster_id" {
+#   value = module.eks.cluster_id
+# }
 
-output "cluster" {
-  value = module.eks
-}
+# output "cluster" {
+#   value = module.eks
+# }
 
-output "self_managed_node_groups" {
-  value = module.eks.self_managed_node_groups
-}
+# output "self_managed_node_groups" {
+#   value = module.eks.self_managed_node_groups
+# }
